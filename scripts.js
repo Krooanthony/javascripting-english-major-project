bebfcca210cc499b765ed59c7744a4193b81499d
fellow <fellow@dhcp-vl2041-3715.eduroam.cornell.edu>
2020-10-01 19:53:27 -0400

    Added prompt to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-alert("script.js has been loaded!");
+let userInput;
+userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
+if ( userInput === "burrito" ) {
+  $("#response").html("Brilliant choice!");
+} else {
+  $("#response").html("Don't your want a burrito?");
+}
