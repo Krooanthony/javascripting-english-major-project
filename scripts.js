fb73089a5df69353abc05b78703bd4e0b4968690
fellow <fellow@dhcp-vl2041-3715.eduroam.cornell.edu>
2020-10-07 18:43:20 -0400

    added tipcalculator to scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,15 @@ for (let i = 1; i <= userInput; i = i + 1) if (i % 2 !== 0) {
   $("#response").append("<br />" + i + " is odd "); } else {
     $("#response").append("<br />" + i + " is even ");
   }
+let tipCalculator;
+tipCalculator = function(total, tipRate){
+  // step 1:
+  let tipAmount;
+  tipAmount = tipRate * total;;
+  // and step 2:
+  $("#response").html("Your tip is $" + tipAmount);
+};
+
+//now call (or "execute") the function, passing a total of $50.00 and a tipRate of 20%
+
+tipCalculator(50.00, 0.2);
