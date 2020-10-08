0d22e17b4dd3b787169684e70d3d1d84788a9e82
fellow <fellow@dhcp-vl2041-3715.eduroam.cornell.edu>
2020-10-08 19:27:01 -0400

    Made a burrito alarm useing objects and arrays

--- scripts.js
+++ scripts.js
@@ -1,18 +1,17 @@
-let userInput;
-userInput = prompt ("Think of a number", "Type your stupid answer here");
-for (let i = 1; i <= userInput; i = i + 1) if (i % 2 !== 0) {
-  $("#response").append("<br />" + i + " is odd "); } else {
-    $("#response").append("<br />" + i + " is even ");
-  }
-let tipCalculator;
-tipCalculator = function(total, tipRate){
-  // step 1:
-  let tipAmount;
-  tipAmount = tipRate * total;;
-  // and step 2:
-  $("#response").html("Your tip is $" + tipAmount);
+let myHabeneroSauceSquirts, myBurritoObject;
+myHabeneroSauceSquirts = 3;
+myBurritoObject = {
+  tortilla: "wheat",
+  guacamole: true,
+  beans: "pinto",
+  habeneroSauceSquirts: myHabeneroSauceSquirts,
+  spiciness: function(){
+    if (myHabeneroSauceSquirts > 0 ){
+      alert("This is a spicy burrito!");
+    } else {
+      alert("This is a mild burrito.");
+    }
+}
 };
-
-//now call (or "execute") the function, passing a total of $50.00 and a tipRate of 20%
-
-tipCalculator(50.00, 0.2);
+$("#response").html("Your burrito has " + myBurritoObject.habeneroSauceSquirts + " squirts of habenro.");
+myBurritoObject.spiciness();
